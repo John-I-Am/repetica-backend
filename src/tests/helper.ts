a233e350ea877fa47fe06e7687790582645cc87b
John He <47208192+John-I-Am@users.noreply.github.com>
2021-10-28 11:00:22 +1300

    Add jest testing

---
+++ src/tests/helper.ts
@@ -0,0 +1,27 @@
+/* eslint-disable no-underscore-dangle */
+import User from '../models/user';
+import Card from '../models/card';
+
+const nonExistingId = async () => {
+  const card = new Card({ front: 'front', back: 'back', level: 1 });
+  await card.save();
+  await card.remove();
+
+  return card._id.toString();
+};
+
+const cardsInDb = async () => {
+  const cards = await Card.find({});
+  return cards.map((card) => card.toJSON());
+};
+
+const usersInDb = async () => {
+  const users = await User.find({});
+  return users.map((u) => u.toJSON());
+};
+
+export default {
+  nonExistingId,
+  cardsInDb,
+  usersInDb,
+};
