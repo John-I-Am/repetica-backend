c5307c930f47dcce9cbb2aef1629fc605d43c9ae
John He <47208192+John-I-Am@users.noreply.github.com>
2021-11-24 16:53:50 +1300

    Fix server connecting to wrong database

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 const { PORT } = process.env;
 
-const MONGODB_URI = process.env.NODE_ENV === ('development' || 'testing')
+const MONGODB_URI = (process.env.NODE_ENV === ('test') || process.env.NODE_ENV === ('development'))
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI;
 
